65759e021fb3b0e5fb40a173f403182df0e696b5
Sakuni1004 <141899881+Sakuni1004@users.noreply.github.com>
2025-02-22 10:01:18 +0530

    Update user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,16 +1,13 @@
-# class User < ApplicationRecord
-#     has_one_attached :file
-
-#     validates :first_name, :last_name, :email, :dob, presence: true
-#     validates :file, attached: true, content_type: ['application/pdf']
-#   end
-  
 class User < ApplicationRecord
   has_one_attached :file
   validates :first_name, :last_name, :email, :dob, presence: true  # Ensure these fields are not empty
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }  # Ensure the email is in the correct format
 
+  validates :file, content_type: { in: ['application/pdf', 'image/png'], message: 'must be a PDF or PNG' }
+
+
   def file_url
     file.attached? ? Rails.application.routes.url_helpers.rails_blob_url(file, only_path: true) : nil
   end
 end
+  
